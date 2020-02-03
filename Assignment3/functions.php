c03b5a8917748bbaf01af792235e11a5a9edc20a
rgrosser-08 <59972112+rgrosser-08@users.noreply.github.com>
2020-02-03 18:18:13 -0500

    Add files via upload
    Assignment 3

---
+++ Assignment3/functions.php
@@ -0,0 +1,15 @@
+<?php
+function jsonToArray(string $file){
+	return json_decode(file_get_contents($file),true);
+}
+function showItem($id,$name,$location,$picture='https://via.placeholder.com/140x100'){
+	 echo '<div class="media">
+              <img src="'.$picture.'" class="mr-3" alt="'.$name.'"width="90">
+              <div class="media-body">
+                <h5 class="mt-0">'.$name.'</h5>
+                located in '.$location.'
+                <br>
+                <a href="details.php?id='.$id.'">About</a>
+              </div>
+              </div>';
+}
